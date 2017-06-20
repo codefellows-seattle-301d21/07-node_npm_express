b4a06ae946df52b9ed1986bd843a403e9e052e8d
Loaye <jr.carstens00@gmail.com>
2017-06-20 16:08:59 -0700

    saved files for push

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,18 +1,22 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
+//DONE: Initialize your project using NPM to create and populate a package.json file
 
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+//DONE: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
+const express = require('express');
 
-// TODO: Include all of the static resources as an argument to app.use()
-
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
+// DONE: Include all of the static resources as an argument to app.use()
+app.use(express.static('./starter-code'));
+// DONE: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(404).send('File NOT Found');
+});
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
