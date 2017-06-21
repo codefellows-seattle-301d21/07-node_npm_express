51c7845db3988d68957591a195346d44bdead7f3
ohjonah <ohjonah@gmail.com>
2017-06-20 17:32:26 -0700

    Adds Alternative Routing
    Adds alternative routing since the wildcard way was a method we went over in class briefly

--- starter-code/server.js
+++ starter-code/server.js
@@ -25,10 +25,6 @@ app.get('/new', function(req, res) {
   res.sendFile('/new.html', {root: './public'});
 });
 
-app.get('*', function(req, res) {
-  res.status(404).send('SUP DAWG. U LIKE 404s?');
-});
-
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
   // and log that form data to the console. We will wire this up soon to actually
@@ -40,4 +36,8 @@ app.post('/articles', bodyParser, function(request, response) {
 app.listen(PORT, function() {
   // TODO: Log to the console a message that lets you know which port your server has started on
   console.log('This port is:', PORT);
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send('SUP DAWG. U LIKE 404s?')
 });
