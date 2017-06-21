ec993c8abd700c615f7e785096f1e05fab38bcc1
Anthony Konschuh <silv.amee@gmail.com>
2017-06-20 17:30:34 -0700

    adds 404 handler and routes new.html

--- starter-code/server.js
+++ starter-code/server.js
@@ -5,14 +5,11 @@ const app = express();
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
 app.use(express.static('./public'));
 
-app.get('/', function(req, res){
-  res.sendFile('./new.html');
+app.get('/new', function(req, res){
+  res.sendFile('/new.html', {root: './public'});
 });
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
@@ -25,4 +22,8 @@ app.post('/articles', bodyParser, function(request, response) {
 app.listen(PORT, function() {
   // TODO: Log to the console a message that lets you know which port your server has started on
   console.log('You are on port:', PORT);
+});
+
+app.use(function(req, res, next) {
+  res.status(404).send('404 File not Found');
 });
